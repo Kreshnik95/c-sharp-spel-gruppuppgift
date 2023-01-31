83efa22a9bc8de63a34dd9bbef8e263433f32b56
Kreshnik95 <114731933+Kreshnik95@users.noreply.github.com>
2023-01-31 18:07:10 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace Guess35
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random newRandom = new Random();
+            int randomNumber = newRandom.Next(1, 36);
+            bool isRunning = true;
+            while (isRunning)
+            {
+                Console.WriteLine("Please guess a number in between 1-35.");
+                if (Int32.TryParse(Console.ReadLine(), out int guess))
+                {
+                    if (guess > randomNumber)
+                    {
+                        Console.WriteLine("Your guess was to high!");
+                    }
+                    else if (guess < randomNumber)
+                    {
+                        Console.WriteLine("Your guess was to low!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("The guess were correct! Congratulation!");
+                        isRunning = false;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("You must enter a valid number!");
+                }
+
+            }
+        }
+    }
+}
+
